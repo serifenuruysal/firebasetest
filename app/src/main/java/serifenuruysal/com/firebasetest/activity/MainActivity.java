2815e3e23eb9ab9f0f19b01c7cde151701a6ed7b
SERIFE NUR UYSAL <serife.uysal@turkcell.com.tr>
2017-07-18 10:14:53 +0300

    first commit

---
+++ app/src/main/java/serifenuruysal/com/firebasetest/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package serifenuruysal.com.firebasetest.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import serifenuruysal.com.firebasetest.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/minSdkIcs/java/com/google/firebase/quickstart/auth/FirebaseUIActivity.java
@@ -0,0 +1,15 @@
+package com.google.firebase.quickstart.auth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import serifenuruysal.com.firebasetest.R;
+
+public class FirebaseUIActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firebase_ui);
+    }
+}
